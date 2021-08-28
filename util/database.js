1491187af8343abd39526e1e7b44b5aedfd56bf9
Adriano Jesus <adriano.jesus2@fatec.sp.gov.br>
2021-08-28 13:23:38 -0300

    Deletes unused js file

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const mongoDb = require('mongoose');
-
-mongoDb
-    .connect('mongodb://localhost:27017/test')
-    .then( () => {console.log('connected mongodb!')})
-    .catch( err => {console.log(err)});
-
-module.exports = mongoDb;
