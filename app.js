3bd998ba70387fe22c5c5f7b6f492bc31f2d7cc8
Adriano Jesus <adriano.jesus2@fatec.sp.gov.br>
2021-08-28 13:23:19 -0300

    Change the name of database

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(assessmentRoutes);
 
 mongoDb
-    .connect('mongodb://mongo1:27017/test?useNewUrlParser=true')
+    .connect('mongodb://mongo1:27017/ms-assessment?useNewUrlParser=true')
     .then( () => {console.log('connected mongodb!')})
     .catch( err => {console.log(err)});
 
